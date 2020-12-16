730514a4efec2d09b1af86abc824d3d37658bab3
jace-ys <jaceys.tan@gmail.com>
2020-12-16 13:25:53 +0800

    [service.control-tower] Add endpoint for publishing commands

--- services/control-tower/src/commands/command.py
+++ services/control-tower/src/commands/command.py
@@ -8,11 +8,11 @@ class CommandEncoder(json.JSONEncoder):
 
 
 class Command:
-    def __init__(self, api_version, protocol):
+    def __init__(self, api_version, protocol, spec={}):
         self.uuid = uuid.uuid4().hex[:16]
         self.apiVersion = api_version
         self.protocol = protocol
-        self.spec = {}
+        self.spec = spec
 
     def json(self):
         return json.dumps(self, indent=4, cls=CommandEncoder)
--- services/control-tower/src/routers/commands.py
+++ services/control-tower/src/routers/commands.py
@@ -1,8 +1,43 @@
-from fastapi import APIRouter
+import typing
+
+from redis import Redis
+from fastapi import APIRouter, HTTPException, status
+from pydantic import BaseModel, validator
+from fastapi.responses import Response
+
+from lib import logger
+from src.commands.command import Command
+from src.commands.publisher import CommandPublisher
+from src.config import config
+
+cfg = config.Config()
+logger = logger.Logger().get()
+
+redis = Redis(host=cfg.redis.HOST, port=cfg.redis.PORT, decode_responses=True)
+publisher = CommandPublisher(logger, redis, None, None)
 
 router = APIRouter()
 
 
-@router.get("/")
-def index():
-    return "Hello, world!"
+class CommandRequest(BaseModel):
+    apiVersion: str
+    protocol: str
+    spec: typing.Dict[str, typing.Any]
+
+    @validator("spec")
+    def spec_not_empty(cls, v):
+        if not v:
+            raise ValueError("spec must not be empty")
+        return v
+
+
+@router.post("/commands")
+async def command(req: CommandRequest):
+    try:
+        cmd = Command(req.apiVersion, req.protocol, req.spec)
+        publisher.publish(cmd)
+        return Response(status_code=status.HTTP_201_CREATED)
+
+    except Exception as err:
+        logger.info("command.publish.failed", error=err)
+        raise HTTPException(status_code=500, detail="failed to publish command")
